6fdfdaa1383600a2a421485c2366a8412283c4d6
ilgyha228 <ilgyha228@gmail.com>
2022-09-28 19:38:31 +0300

    adding AppDbContext

---
+++ DAL/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+
+namespace DAL.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<ReceiptDetail> ReceiptsDetails { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+    
